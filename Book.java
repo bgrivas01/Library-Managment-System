80323884a073d76ea8d33802dc4043c5cc57e193
sn0wcode <bgrivas16@gmail.com>
2026-03-07 11:59:53 -0600

    added Book class

---
+++ Book.java
@@ -0,0 +1,33 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isBoorrowed;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isBoorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isBorrowed() {
+        return isBoorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBoorrowed = borrowed;
+    }
+}
